ded4ab6fa61d45d641109959de4096cb171d0b81
takuan517 <ishitaku-2000517@keio.jp>
2020-01-08 01:11:11 +0900

    ユーザー編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,32 @@ class UsersController < ApplicationController
     redirect_to "/"
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_update_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+    redirect_to root_path
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:email,:password,:password_confirmation)
+  end
+  def user_update_params
+    params.require(:user).permit(:email)
   end
 end
