70958179a58cf37fb47b6b2bda8687a94cb40d55
takuan517 <ishitaku-2000517@keio.jp>
2020-01-08 01:23:47 +0900

    ログイン機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save
-    redirect_to "/"
+    log_in @user
+    redirect_to @user
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/index'
   get 'users/new'
   get 'users/create'
   resources :users
   root "users#index"
+  resources :sessions, only: [:new,:create,:destroy]
 end
